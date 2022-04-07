2e3de32ba9f50b59392fc88ef99219769913fa4e
Watcha Sasawattakul <watcha.sasawattakul@gmail.com>
2022-04-07 13:56:11 +0700

    add API specification document by swagger

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -6,6 +6,61 @@ const router = express.Router()
 const appointmentRouter=require('./appointments');
 
 const {protect,authorize} = require('../middleware/auth');
+const swaggerJsdoc = require('swagger-jsdoc');
+const { UCS2_UNICODE_CI } = require('mysql/lib/protocol/constants/charsets');
+const Hospital = require('../models/Hospital');
+
+
+/**
+* @swagger
+* components:
+*    schemas:
+*        Hospitals:
+*            type: object
+*            required:
+*               -   name
+*               -   address
+*            properties:
+*                id:
+*                    type: string
+*                    format: uuid
+*                    description: The auto-generated id of the Hospital
+*                    example: d290f1ee-6c54-4b01-90e6-d701748f0851
+*                ลำดับ:
+*                    type: string
+*                    description: Oridinal number
+*                name:
+*                    type: string
+*                    description: Hospital name
+*                address:
+*                    type: string
+*                    description: House No., Street, Road
+*                district:
+*                    type: string
+*                    description: District
+*                province:
+*                    type: string
+*                    description: province
+*                postalcode:
+*                    type: string
+*                    description: 5-digit postal code
+*                tel:
+*                    type: string
+*                    description: telephone number
+*                region:
+*                    type: string
+*                    description: region
+*            example:
+*                id: 609bda561452242d88d36e37
+*                ลำดับ: 121
+*                name: Happy Hospital
+*                address: 121 ถ.สุขุมวิท
+*                district: บางนา
+*                province: กรุงเทพมหานคร
+*                postalcode: 10110
+*                tel: 02-2187000
+*                region: กรุงเทพมหานคร (Bangkok)
+*/
 
 //Re-route into other resource routers
 router.use('/:hospitalId/appointments/',appointmentRouter);
@@ -15,3 +70,129 @@ router.route('/vacCenters').get(getVacCenters);
 router.route('/:id').get(getHospital).put(protect, authorize('admin'),updateHospital).delete(protect,authorize('admin'), deleteHospital);
 
 module.exports=router;
+
+
+
+/**
+ * @swagger
+ * tags:
+ *  name: Hospitals
+ *  description: The hospitals managing API
+ */
+
+/**
+ * @swagger
+ * /hospitals:
+ *  get:
+ *      summary: Returns the list of all the hospitals
+ *      tags: [Hospitals]
+ *      responses:
+ *          200:
+ *              description: The list of the hospitals
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Hospitals'
+ */
+
+/**
+ * @swagger
+ * /hospitals/{id}:
+ *  get:
+ *      summary: Get the hospital by id
+ *      tags: [Hospitals]
+ *      parameters:
+ *          -   in: path
+ *              name: id
+ *              schema:
+ *                  type: string
+ *              required: true
+ *              description: The hospital id
+ *      responses:
+ *          200:
+ *              description: The hospital description by id
+ *              contents:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Hospitals'
+ *          404:
+ *              desciption: The hospital was not found
+ */
+
+/**
+ * @swagger
+ * /hospitals:
+ *  post:
+ *      summary: Create a new hospital
+ *      tags: [Hospitals]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Hospitals'
+ *      responses:
+ *          201:
+ *              description: The hospital was successfully created
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Hospitals'
+ *          500:
+ *              description: Some server error
+ */
+
+/**
+ * @swagger
+ * /hospitals/{id}:
+ *   put:
+ *      summary: Update the hospital by the id
+ *      tags: [Hospitals]
+ *      parameters:
+ *          -   in: path
+ *              name: id
+ *              schema:
+ *                  type: string
+ *              required: true
+ *              description: The hospital id
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Hospitals'
+ *      responses:
+ *          200:
+ *              description: The hospital was updated
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Hospitals'
+ *          404:
+ *              description: The hospital was not found
+ *          500:
+ *              description: Some server error
+ */
+
+/**
+ * @swagger
+ * /hospitals/{id}:
+ *  delete:
+ *      summary: Remove the hospital by id
+ *      tags: [Hospitals]
+ *      parameters:
+ *          -   in: path
+ *              name: id
+ *              schema:
+ *                  type: string
+ *              required: true
+ *              description: The hospital id
+ * 
+ *      responses:
+ *          200:
+ *              description: The hospital was deleted
+ *          404:
+ *              description: The hospital was not found
+ */
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const xss=require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp=require('hpp');
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -17,6 +20,27 @@ connectDB();
 //Body parser
 const app=express();
 app.use(express.json());
+
+//swagger
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/api/v1'
+            }
+        ]
+        
+    },
+    apis:['./routes/*.js']
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Cookie parser
 app.use(cookieParser());
@@ -50,6 +74,8 @@ app.use(hpp());
 app.use('/api/v1/hospitals',hospitals);
 app.use('/api/v1/auth',auth);
 app.use('/api/v1/appointments', appointments);
+
+
 
 // app.get('/', (req,res) => {
 //     // res.send("<h1>Hello from express</h1>");
